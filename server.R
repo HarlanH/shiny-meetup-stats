bc1abc992c9257927a4245e55246807d15dd6b3a
Harlan Harris <harlan@harris.name>
2016-01-03 19:37:18 -0500

    add nametags tab

--- server.R
+++ server.R
@@ -4,6 +4,9 @@ library(httr)
 library(tidyr)
 library(ggplot2)
 library(dplyr)
+library(stringr)
+
+options(stringsAsFactors = FALSE)
 
 source("auth.R")
 
@@ -55,6 +58,15 @@ shinyServer(function(input, output, session) {
     }
   })
   
+  ################### sidebar #############################
+  
+  observe({
+    message("updateSelectedMeetup")
+    updateSelectInput(session, "selectedMeetup", choices = userMeetups())
+  })
+  
+  ################### API queries #############################
+  
   userMeetups <- reactive({
     message("userMeetups")
     tok <- AccessToken()
@@ -84,6 +96,92 @@ shinyServer(function(input, output, session) {
     }
   })
   
+  upcomingEvents <- reactive({
+    if (nchar(input$selectedMeetup) > 0) {
+      tok <- AccessToken()
+      resp <- GET(url=paste0(MeetupBaseURL, "/2/events"),
+                  query=list(group_urlname=input$selectedMeetup, 
+                             status="upcoming",
+                             only="name,id,time,yes_rsvp_count",
+                             access_token=tok))
+      res <- content(resp)$results
+      data.frame(name=sapply(res, function(x) x$name),
+                 id=sapply(res, function(x) x$id),
+                 time=as.POSIXct(sapply(res, function(x) x$time)/1e3, 
+                                 origin=Sys.time() - as.numeric(Sys.time())),
+                 yes_rsvp_count=sapply(res, function(x) x$yes_rsvp_count)
+      )
+    }
+  })
+  
+  nextEvent <- reactive({
+    upcomingEvents() %>% arrange(time) %>% head(1)
+  })
+  
+  getSomeRSVPs <- function(tok, event.id, offset=0, page=200) {
+    resp <- GET(url=paste0(MeetupBaseURL, "/2/rsvps"),
+                query=list(group_urlname=input$selectedMeetup, 
+                           rsvp="yes",
+                           event_id=event.id,
+                           page=page, offset=offset,
+                           only="member",
+                           access_token=tok))
+    res <- content(resp)$results
+    sapply(res, function(x) x$member$member_id)
+  }
+  
+  # get a list of members attending the next event
+  nextEventRSVPs <- reactive({
+    if (nchar(input$selectedMeetup) > 0) {
+      tok <- AccessToken()
+      event_rsvps <- nextEvent()$yes_rsvp_count
+      page.size = 200 # could be higher?
+      
+      message("getting RSVPs")
+      member_ids <- sapply(seq.int(from=0,to=floor(event_rsvps/page.size)),
+                          function(o) getSomeRSVPs(tok, nextEvent()$id, 
+                                                   offset=o, page=page.size))
+      member_ids <- unlist(member_ids)
+      message("got ", length(member_ids), " RSPVs")
+      member_ids
+    }
+  })
+  
+  getSomeProfiles <- function(tok, id_str, offset=0, page=20) {
+    resp <- GET(url=paste0(MeetupBaseURL, "/2/profiles"),
+                query=list(group_urlname=input$selectedMeetup, 
+                           member_id=id_str,
+                           page=page, offset=offset,
+                           only="name,role,title",
+                           access_token=tok))
+    res <- content(resp)$results
+    data.frame(name=sapply(res, function(x) x$name),
+               role=sapply(res, function(x) if ('role' %in% names(x)) x$role else ''),
+               title=sapply(res, function(x) if ('title' %in% names(x)) x$title else ''))
+  }
+  
+  # get profile info for VIPs at next event
+  nextEventVIPs <- reactive({
+    if (nchar(input$selectedMeetup) > 0) {
+      tok <- AccessToken()
+      member_ids <- nextEventRSVPs()
+      
+      member_ids_df <- data.frame(member_ids, group=floor(seq_along(member_ids)/20))
+      message("getting profiles")
+      profiles <- member_ids_df %>% group_by(group) %>%
+        do(getSomeProfiles(tok, paste(.$member_ids, collapse=','))) %>%
+        ungroup() %>%
+        mutate(titlerole = ifelse(role != '', role, title)) %>%
+        filter(titlerole != '') %>%
+        select(name, titlerole)
+      message("got ", nrow(profiles), " VIP profiles")
+      
+      profiles
+    }
+  })
+  
+  ################### RSVP Plot #############################
+  
   output$rsvpPlot <- renderPlot({
     message("rsvpPlot")
     validate(
@@ -101,8 +199,14 @@ shinyServer(function(input, output, session) {
       geom_area(position="identity", alpha=.5) 
   })
   
-  observe({
-    message("updateSelectedMeetup")
-    updateSelectInput(session, "selectedMeetup", choices = userMeetups())
-  })
+  ################### Nametags #############################
+  output$downloadNametagsCSV <- downloadHandler(
+    filename = function() {
+      sprintf("%s-%s.csv", input$selectedMeetup, make.names(str_sub(nextEvent()$name, end=20)))
+    },
+    content = function(con) {
+      write.csv(nextEventVIPs(), con)
+    }
+  )
+  
 })
--- ui.R
+++ ui.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(shinydashboard)
 
+options(stringsAsFactors = FALSE)
+
 shinyUI(dashboardPage(
   dashboardHeader(title="Meetup Stats"),
   dashboardSidebar(
@@ -8,6 +10,9 @@ shinyUI(dashboardPage(
     selectInput("selectedMeetup", "Group", ""),
     sidebarMenu(
       menuItem("RSVPs", tabName="rsvps", icon=icon("thumbs-up")),
+      menuItem("Nametags", tabName="nametags", icon=icon("tags")),
+      menuItem("Attendees", tabName="attendees", icon=icon("users")),
+      menuItem("Pick Winner", tabName="pickwinner", icon=icon("trophy")),
       menuItem("About", tabName="about", icon=icon("question-circle"))
     )
   ),
@@ -18,6 +23,12 @@ shinyUI(dashboardPage(
     tabItems(
       tabItem("rsvps",
               plotOutput("rsvpPlot")),
+      tabItem("nametags",
+              downloadButton("downloadNametagsCSV", label="Download Nametag CSV")),
+      tabItem("attendees",
+              plotOutput("attendeeClusters")),
+      tabItem("pickwinner",
+              actionButton("winnerbutton", "Pick a Winner!", icon=icon("trophy"))),
       tabItem("about",
               a(href="https://github.com/HarlanH/shiny-meetup-stats", "Source on Github"))
     )
